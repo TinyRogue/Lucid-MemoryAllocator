220a33e4749b6f62ca6a23ddf45bbd621c5fbfa4
TinyRogue <oskarmikus@protonmail.com>
2020-11-29 22:32:33 +0100

    Added heap_validate() constans as macros

--- heap.h
+++ heap.h
@@ -1,17 +1,18 @@
 #ifndef HEAP_H
 #define HEAP_H
 
-#include <stdlib.h>         /* For size_t, could be replaced with <stddef.h>, <stdio.h>, <string.h>, <time.h>, <wchar.h> */
-#include <stdbool.h>        /* For bool type */
-#include <string.h>         /* For memcpy() */
-#include <stdio.h>          /* For logging with printf funcs */
-#include <inttypes.h>       /* For uintptr_t */
-#include "custom_unistd.h"  /* For (custom_)sbrk function, required for project */
+#include <stdlib.h>                 /* For size_t, could be replaced with <stddef.h>, <stdio.h>, <string.h>, <time.h>, <wchar.h> */
+#include <stdbool.h>                /* For bool type */
+#include <string.h>                 /* For memcpy() */
+#include <stdio.h>                  /* For logging with printf funcs */
+#include <inttypes.h>               /* For uintptr_t */
+#include "custom_unistd.h"          /* For (custom_)sbrk function, required for project */
+#include "display_dependencies.h"   /* For colourful terminal messages */
 
 
 #define FENCE_LENGTH 0x3
 #define KB 0x400
-#define PAGE_SIZE 0x1000
+#define MY_PAGE_SIZE 0x1000
 #define CONTROL_STRUCT_SIZE sizeof(Header__)
 #define HEADER_SIZE(size) (CONTROL_STRUCT_SIZE + size + 2 * FENCE_LENGTH)
 
@@ -19,12 +20,16 @@
 #define HEAP_INIT_FAIL -1
 #define REQUEST_SPACE_FAIL HEAP_INIT_FAIL
 
+#define HEAP_CORRUPTED 1
+#define HEAP_UNINITIALIZED 2
+#define HEAP_CONTROL_STRUCT_BLUR 3
+
 struct header_t {
     struct header_t *prev;
     struct header_t *next;
     size_t mem_size;
     bool is_free;
-    void *user_memptr;
+    void *user_mem_ptr;
 } __attribute__((packed));
 
 typedef struct header_t Header__;
@@ -58,12 +63,12 @@ void* heap_calloc(size_t number, size_t size);
 void* heap_realloc(void* memblock, size_t count);
 void heap_free(void* memblock);
 
-void* heap_malloc_aligned(size_t count);                     
-void* heap_calloc_aligned(size_t number, size_t size);       
-void* heap_realloc_aligned(void* memblock, size_t size);  
+void* heap_malloc_aligned(size_t count);
+void* heap_calloc_aligned(size_t number, size_t size);
+void* heap_realloc_aligned(void* memblock, size_t size);
 
 size_t heap_get_largest_used_block_size(void);
-enum pointer_type_t get_pointer_type(const void* const pointer);
+enum pointer_type_t get_pointer_type(const void* pointer);
 
 void display_heap();
 
